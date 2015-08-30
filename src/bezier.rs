af26ff7f2355431932df131c1d6cbdc81d802910
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-30 11:35:10 +0200

    Generalize over floating-point types

--- src/bezier.rs
+++ src/bezier.rs
@@ -1,44 +1,65 @@
 //! Bézier curves.
 
+use num::Float;
+
 use {Curve, Trace};
 
 /// A cubic Bézier curve.
 #[derive(Clone, Copy, Debug)]
-pub struct Cubic {
-    x0: f64,
-    y0: f64,
-    x1: f64,
-    y1: f64,
-    x2: f64,
-    y2: f64,
-    x3: f64,
-    y3: f64,
+pub struct Cubic<T: Float> {
+    x0: T,
+    y0: T,
+    x1: T,
+    y1: T,
+    x2: T,
+    y2: T,
+    x3: T,
+    y3: T,
 }
 
-impl Cubic {
+impl<T: Float> Cubic<T> {
     /// Create a curve.
     #[inline]
-    pub fn new(x0: f64, y0: f64, x1: f64, y1: f64, x2: f64, y2: f64, x3: f64, y3: f64) -> Cubic {
+    pub fn new(x0: T, y0: T, x1: T, y1: T, x2: T, y2: T, x3: T, y3: T) -> Cubic<T> {
         Cubic { x0: x0, y0: y0, x1: x1, y1: y1, x2: x2, y2: y2, x3: x3, y3: y3 }
     }
-
-    /// Start tracing the curve.
-    #[inline]
-    pub fn trace<'l>(&'l self, steps: usize) -> Trace<'l, Self> {
-        Trace { curve: self, steps: steps, step: 0 }
-    }
 }
 
-impl Curve for Cubic {
-    fn evaluate(&self, t: f64) -> (f64, f64) {
-        debug_assert!(0.0 <= t && t <= 1.0);
-        let t2 = t * t;
-        let t3 = t2 * t;
-        let ct = 1.0 - t;
-        let ct2 = ct * ct;
-        let ct3 = ct2 * ct;
-        let x = ct3 * self.x0 + 3.0 * ct2 * t * self.x1 + 3.0 * ct * t2 * self.x2 + t3 * self.x3;
-        let y = ct3 * self.y0 + 3.0 * ct2 * t * self.y1 + 3.0 * ct * t2 * self.y2 + t3 * self.y3;
-        (x, y)
-    }
+macro_rules! implement {
+    ($float:ty) => (
+        impl Cubic<$float> {
+            /// Start tracing the curve.
+            #[inline(always)]
+            pub fn trace<'l>(&'l self, steps: usize) -> Trace<'l, $float, Self> {
+                Trace::new(self, steps)
+            }
+        }
+
+        impl Curve<$float> for Cubic<$float> {
+            fn evaluate(&self, t: $float) -> ($float, $float) {
+                debug_assert!(0.0 <= t && t <= 1.0);
+
+                let t2 = t * t;
+                let t3 = t2 * t;
+                let ct = 1.0 - t;
+                let ct2 = ct * ct;
+                let ct3 = ct2 * ct;
+
+                let x = ct3 * self.x0 +
+                        3.0 * ct2 * t * self.x1 +
+                        3.0 * ct * t2 * self.x2 +
+                        t3 * self.x3;
+
+                let y = ct3 * self.y0 +
+                        3.0 * ct2 * t * self.y1 +
+                        3.0 * ct * t2 * self.y2 +
+                        t3 * self.y3;
+
+                (x, y)
+            }
+        }
+    );
 }
+
+implement!(f32);
+implement!(f64);
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,51 @@
 //! Curves.
 
+extern crate num;
+
+use num::Float;
+use std::marker::PhantomData;
+
 /// A type identifying itself as a curve.
-pub trait Curve {
+pub trait Curve<T: Float> {
     /// Evalute the curve at a point in `[0, 1]`.
-    fn evaluate(&self, f64) -> (f64, f64);
+    fn evaluate(&self, T) -> (T, T);
 }
 
 /// A trace of a curve.
 #[derive(Clone, Copy, Debug)]
-pub struct Trace<'l, T: 'l + Curve> {
-    curve: &'l T,
+pub struct Trace<'l, T: Float, C: 'l + Curve<T>> {
+    curve: &'l C,
     steps: usize,
-    step: usize,
+    position: usize,
+    phantom: PhantomData<T>,
 }
 
-impl<'l, T: Curve> Iterator for Trace<'l, T> {
-    type Item = (f64, f64);
-
-    fn next(&mut self) -> Option<(f64, f64)> {
-        let step = self.step;
-        if step < self.steps {
-            self.step += 1;
-            Some(self.curve.evaluate(step as f64 / ((self.steps - 1) as f64)))
-        } else {
-            None
-        }
+impl<'l, T: Float, C: Curve<T>> Trace<'l, T, C> {
+    #[inline(always)]
+    fn new(curve: &'l C, steps: usize) -> Self {
+        Trace { curve: curve, steps: steps, position: 0, phantom: PhantomData }
     }
 }
+
+macro_rules! implement {
+    ($float:ty) => (
+        impl<'l, T: Curve<$float>> Iterator for Trace<'l, $float, T> {
+            type Item = ($float, $float);
+
+            fn next(&mut self) -> Option<Self::Item> {
+                let position = self.position;
+                if position < self.steps {
+                    self.position += 1;
+                    Some(self.curve.evaluate(position as $float / (self.steps - 1) as $float))
+                } else {
+                    None
+                }
+            }
+        }
+    );
+}
+
+implement!(f32);
+implement!(f64);
 
 pub mod bezier;
